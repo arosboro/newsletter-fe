6af1f23319c3531da2fc3e1f1ed12fb97a02b64d
Andrew Rosborough <arosborough@oculat.us>
2023-08-09 12:36:58 -0400

    Resolve wasm-pack resolution.

--- install.sh
+++ install.sh
@@ -3,7 +3,16 @@ echo "Installing Rustup..."
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 amazon-linux-extras install rust1
 # Adding binaries to path
-source "/root/.cargo/env"
+# rustup shell setup
+# affix colons on either side of $PATH to simplify matching
+case ":${PATH}:" in
+    *:"$HOME/.cargo/bin":*)
+        ;;
+    *)
+        # Prepending path in case a system-installed rustc needs to be overridden
+        export PATH="$HOME/.cargo/bin:$PATH"
+        ;;
+esac
 echo "Installing wasm-pack..."
 # Install wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
