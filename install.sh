827e66cfa777b611b27c1d60175f83e20d6cc6e8
Andrew Rosborough <arosborough@oculat.us>
2023-08-09 13:54:37 -0400

    Added steps for rustup.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 echo "Installing Rust..."
 amazon-linux-extras install rust1
 echo "Updating PATH for cargo binaries."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 PATH=$PATH:$HOME/.cargo/bin
 echo "Installing wasm-pack..."
 cargo install wasm-pack
