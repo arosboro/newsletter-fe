dc67a45791699520f5f716f63e7748d927172d31
Andrew Rosborough <arosborough@oculat.us>
2023-08-09 13:39:00 -0400

    Update environment.

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 echo "Installing Rust..."
 amazon-linux-extras install rust1
+echo "Updating PATH for cargo binaries."
+PATH=$PATH:$HOME/.cargo/bin
 echo "Installing wasm-pack..."
 cargo install wasm-pack
 echo "Building wasm..."
