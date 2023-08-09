4ed396bbe5eb4814a86a39161eec115556976e5e
Andrew Rosborough <arosborough@oculat.us>
2023-08-09 15:30:46 -0400

    Update target.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ echo "Updating PATH for cargo binaries."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 PATH=$PATH:$HOME/.cargo/bin
 echo "Installing wasm-pack..."
+rustup target add wasm32-unknown-unknown
 cargo install wasm-pack
 echo "Building wasm..."
 yarn run wasm
