24e7ddcf9a296ced4a08b55adeacefe1e72594dd
Andrew Rosborough <arosborough@oculat.us>
2023-08-09 12:16:33 -0400

    Resolve rust installation on setup.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 echo "Installing Rustup..."
 # Install Rustup (compiler)
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# source "$HOME/.cargo/env"
 amazon-linux-extras install rust1
 # Adding binaries to path
-source "$HOME/.cargo/env"
 echo "Installing wasm-pack..."
 # Install wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
