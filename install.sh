5f4f10549d66722e147bdab0b088d68d402d0f96
Andrew Rosborough <arosborough@oculat.us>
2023-08-10 18:38:34 -0400

    Update rustup.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ PATH=$PATH:$HOME/.cargo/bin
 echo "Installing wasm-pack..."
 # Determine your Rust version
 echo `rustc --version`
+cargo clean
 RUST_VERSION="1.68.2"
 # Download the correct Wasm32 target
 curl -O "https://static.rust-lang.org/dist/rust-std-$RUST_VERSION-wasm32-unknown-unknown.tar.gz"
@@ -13,7 +14,6 @@ tar -xzvf "rust-std-$RUST_VERSION-wasm32-unknown-unknown.tar.gz"
 # Move the wasm32-unknown-unknown folder to the sysroot
 SYSROOT_PATH=$(rustc --print sysroot)
 mv "rust-std-$RUST_VERSION-wasm32-unknown-unknown/rust-std-wasm32-unknown-unknown/lib/rustlib/wasm32-unknown-unknown" "$SYSROOT_PATH/lib/rustlib/"
-cargo clean
 cargo install wasm-pack
 echo "Building wasm..."
 yarn run wasm
