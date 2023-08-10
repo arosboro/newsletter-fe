1075744ed3f1636a728c968f0fee72f291bf6ae5
Andrew Rosborough <arosborough@oculat.us>
2023-08-10 16:16:03 -0400

    Update rustup.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 echo "Installing Rust..."
 amazon-linux-extras install rust1
-echo "Updating PATH for cargo binaries."
+echo "Updating PATH for cargo binaries..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 PATH=$PATH:$HOME/.cargo/bin
-rustup update
+rustup update -y
 echo "Installing wasm-pack..."
 rustup target add wasm32-unknown-unknown
 cargo install wasm-pack
