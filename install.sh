b93bbb276f0530830ed23e7973e0834ef48521d2
Andrew Rosborough <arosborough@oculat.us>
2023-08-10 16:27:37 -0400

    Update rustup.

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 echo "Installing Rust..."
 amazon-linux-extras install rust1
-curl https://sh.rustup.rs -sSf | sh -s -- -y
 echo "Updating PATH for cargo binaries..."
 PATH=$PATH:$HOME/.cargo/bin
 echo "Installing wasm-pack..."
-rustup target add wasm32-unknown-unknown
+# Determine your Rust version
+RUST_VERSION="1.33.0"
+# Download the correct Wasm32 target
+wget "https://static.rust-lang.org/dist/rust-std-$RUST_VERSION-wasm32-unknown-unknown.tar.gz"
+# Unpack the tarball
+tar -xzvf "rust-std-$RUST_VERSION-wasm32-unknown-unknown.tar.gz"
+# Move the wasm32-unknown-unknown folder to the sysroot
+SYSROOT_PATH=$(rustc --print sysroot)
+mv "rust-std-$RUST_VERSION-wasm32-unknown-unknown/rust-std-wasm32-unknown-unknown/lib/rustlib/wasm32-unknown-unknown" "$SYSROOT_PATH/lib/rustlib/"
 cargo install wasm-pack
 echo "Building wasm..."
 yarn run wasm
