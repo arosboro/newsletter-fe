3e4821d49079d76a05ca67581a091f5fa1688975
Andrew Rosborough <arosborough@oculat.us>
2023-08-10 19:36:32 -0400

    Update.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo "Installing Rust..."
 # amazon-linux-extras install rust1
 export CARGO_HOME=/vercel/.cargo
 export RUSTUP_HOME=/vercel/.rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 echo "Updating PATH for cargo binaries..."
 PATH=$PATH:$HOME/.cargo/bin
 echo "Installing wasm-pack..."
