60805125919252fc946ed6da8583ff35dbaa6968
Andrew Rosborough <arosborough@oculat.us>
2023-08-10 20:09:43 -0400

    Update typescript.

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 echo "Installing Rust..."
 # amazon-linux-extras install rust1
+export HOME=/vercel
 export CARGO_HOME=/vercel/.cargo
 export RUSTUP_HOME=/vercel/.rustup
-sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 echo "Updating PATH for cargo binaries..."
 PATH=$PATH:$HOME/.cargo/bin
 echo "Installing wasm-pack..."
 cargo install wasm-pack
 echo "Building wasm..."
+yarn global add typescript
 yarn run wasm
