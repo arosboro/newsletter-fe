81aad7075c5dfc27475a2df1b07e4b1c6d547efc
Andrew Rosborough <arosborough@oculat.us>
2023-08-10 20:36:51 -0400

    Syntax.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "Installing Rust with rustup..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y  2> /dev/null
-"Configuring shell prior to installing rust..."
+echo "Configuring shell prior to installing rust..."
 source "$HOME/.cargo/env"
 npm install
