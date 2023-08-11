27223bcea54025858355bbd61cfe29865e86a34f
Andrew Rosborough <arosborough@oculat.us>
2023-08-10 20:35:39 -0400

    Rely on npm install.

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 echo "Installing Rust with rustup..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y  2> /dev/null
-echo "Updating PATH for cargo binaries..."
-PATH=$PATH:$HOME/.cargo/bin
-echo "Installing wasm-pack..."
-cargo install wasm-pack
-echo "Installing typescript..."
-npm install -g typescript
+"Configuring shell prior to installing rust..."
+source "$HOME/.cargo/env"
+npm install
