6bd9308f06339b713e232650d476182a70665bb2
Andrew Rosborough <arosborough@oculat.us>
2023-08-10 20:40:00 -0400

    Install wasm-pack.

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ echo "Installing Rust with rustup..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y  2> /dev/null
 echo "Configuring shell prior to installing rust..."
 source "$HOME/.cargo/env"
+echo "Installing wasm-pack..."
+cargo install wasm-pack
 npm install
