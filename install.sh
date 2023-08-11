0ae391d95540e78d9271747a89298eb95a49c01f
Andrew Rosborough <arosborough@oculat.us>
2023-08-10 20:20:45 -0400

    Add package-lock.json.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ export HOME=/vercel
 export CARGO_HOME=/vercel/.cargo
 export RUSTUP_HOME=/vercel/.rustup
 PATH=$PATH:$HOME/.cargo/bin
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y  2> /dev/null
 echo "Updating PATH for cargo binaries..."
 echo "Installing wasm-pack..."
 cargo install wasm-pack
